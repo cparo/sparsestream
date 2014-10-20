086283b6a77ed2e90f7bf83bc1f2b12e2821a2d5
Christian Paro <cparo@digitalocean.com>
2014-10-20 03:15:58 -0400

    Add commentary about inlining in header files
    ...since that can get very ugly very fast in larger projects, and is
    done here to hint to human readers at what the compiler would be
    expected to do on its own accord rather than as something which is
    necessary to get the compiler to optimize away the overhead of calling
    a three-line function run in inner-loop code.

--- sparseencode.h
+++ sparseencode.h
@@ -7,6 +7,18 @@
 uint64_t buffer[BUFFER_ELEMENTS] = {0};
 
 // Check for whether the buffer contains anything other than zero/null bytes.
+// This works with a simple "inline" declaration with no corresponding "extern"
+// anywhere else because no object derived from this header is also derived from
+// more than one c file using this function (doing so would induce a "multiple
+// declaration" error at compile time). Explicitly hinting that this function
+// should be inligned isn't of much use to the compiler (which generally is very
+// good at making such optimizations on its own when it is appropriate to do
+// do), but it does clarify to human readers that the intent for this function
+// is merely to avoid source duplication rather than for it to actually be
+// compiled as a "real" non-inline function. Compiler hints in more complex
+// projects can themselves become complex and subtle to work with - and are
+// generally best avoided when not found to offer any significant improvement
+// through profiling/testing.
 inline int buffer_is_used()
 {
 	uint64_t i, rc;
@@ -19,7 +31,7 @@ inline int buffer_is_used()
 	// instead of simply using an if/else to return true upon encountering a
 	// non-zero value as this has been found to reduce user-space computational
 	// cost by about 25-30% for zero-filled blocks and increase it by about 20%
-	// for randomly-filled blocks relative to to more obvious method (likely
+	// for randomly-filled blocks relative to to the more obvious method (likely
 	// because of the avoidance of branching within the loop). Since this
 	// encoder is being used because the input data is expected to generally
 	// have a high proportion of empty space, favoring reduced overhead for
